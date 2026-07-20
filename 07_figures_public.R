7319d5440a61f9023830df4aae320401cd54f0e3
CECCTHOR <167721561+CECCTHOR@users.noreply.github.com>
2026-07-20 13:49:54 -0500

    Delete 07_figures_public.R

--- 07_figures_public.R
+++
@@ -1,4 +0,0 @@
-# Public figure inventory check.
-figs <- list.files("figures", pattern = "\\.(png|jpg|tiff)$", full.names = TRUE)
-stopifnot(length(figs) >= 10)
-message("Public figures available: ", length(figs))
